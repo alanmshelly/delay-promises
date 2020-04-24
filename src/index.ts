1c202a50750499fadfff4a5cc8cb49792790a705
Alan Shelly <alan.m.shelly@gmail.com>
2020-04-24 11:50:24 +0900

    Added implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-export class DelayedPromise {
+export type DelayedPromise<T> = {
+    resolve: (value: T | PromiseLike<T>) => void
+    reject: (reason?: any) => void
+} & Promise<T>
 
+export function createDelayedPromise<T>(): DelayedPromise<T> {
+    let resolveFunction: (value: T | PromiseLike<T>) => void
+    let rejectFunction: (reason?: any) => void
+
+    const promise = new Promise(((resolve, reject) => {
+        resolveFunction = resolve
+        rejectFunction = reject
+    })) as DelayedPromise<T>
+
+    promise.resolve = (value: T | PromiseLike<T>) => resolveFunction(value)
+    promise.reject = (reason?: any) => rejectFunction(reason)
+
+    return promise
 }
-
--- tests/DelayedPromise.test.ts
+++ tests/DelayedPromise.test.ts
@@ -1,7 +1,74 @@
-import {DelayedPromise} from '../src'
+import {createDelayedPromise, DelayedPromise} from '../src'
 
 describe('DelayedPromise', () => {
-    it('create a new delayed promise', () => {
-        expect(new DelayedPromise()).toBeDefined()
+    it('should be a Promise', () => {
+        expect(createDelayedPromise().constructor).toBe(Promise)
+    })
+
+    it('should not resolve or reject', async () => {
+        let promiseResolvedOrRejected = false
+        let promiseCompleted = false
+        createDelayedPromise()
+            .then(() => {
+                promiseResolvedOrRejected = true
+            })
+            .catch(() => {
+                promiseResolvedOrRejected = true
+            })
+            .finally(() => {
+                promiseCompleted = true
+            })
+
+        await wait()
+
+        expect(promiseResolvedOrRejected).toEqual(false)
+        expect(promiseCompleted).toEqual(false)
+    })
+
+    it('should resolve with value when resolve is called', async () => {
+        let promiseResolveValue: string | null = null
+        let promiseCompleted = false
+        let delayedPromise = createDelayedPromise<string>()
+        delayedPromise
+            .then((value) => {
+                promiseResolveValue = value
+            })
+            .finally(() => {
+                promiseCompleted = true
+            })
+
+
+        delayedPromise.resolve('promise resolved with value')
+
+        await wait()
+
+        expect(promiseResolveValue).toEqual('promise resolved with value')
+        expect(promiseCompleted).toEqual(true)
+    })
+
+    it('should reject with value when reject is called', async () => {
+        let promiseRejectedValue: string | null = null
+        let promiseCompleted = false
+        let delayedPromise = createDelayedPromise()
+
+        delayedPromise
+            .catch((value) => {
+                promiseRejectedValue = value
+            })
+            .finally(() => {
+                promiseCompleted = true
+            })
+
+
+        delayedPromise.reject('promise rejected with value')
+
+        await wait()
+
+        expect(promiseRejectedValue).toEqual('promise rejected with value')
+        expect(promiseCompleted).toEqual(true)
     })
 })
+
+function wait() {
+    return new Promise(resolve => setTimeout(resolve, 0))
+}
